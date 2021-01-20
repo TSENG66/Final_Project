1cedc174429becf59c333f4c6ea537ea9659dd62
曾育詳 <tsengyuhsiang@cengyuxiangde-MacBook-Air.local>
2021-01-21 01:05:35 +0800

    remove cors in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import cors from 'cors'
+// import cors from 'cors'
 import routes from './routes'
 import mongoose from 'mongoose'
 
@@ -7,7 +7,7 @@ require('dotenv').config()
 const app = express()
 
 // init middleware
-app.use(cors())
+// app.use(cors())
 app.use(express.json())
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4000')
